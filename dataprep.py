07b6d072fdd6364602653a859c1ae34c185e7cb9
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-09-19 15:45:06 -0300

    Created empty preprocessing files
    dataprep.py, to contain functions needed
    dataprep.ipynb for real preprocessing
    & borrador.ipynb for trial and error

---
+++ dataprep.py
@@ -0,0 +1,12 @@
+'''
+Librería de preprocesamiento EEG
+--
+Ammi Beltrán & Fernanda Borja
+'''
+# Required libraries
+import mne
+import os
+
+# FOR TESTING
+EDFDIR = "D:\\OneDrive\\OneDrive - Universidad de Chile\\Semestre X\\Inteligencia\\Proyecto\\dataset\\tuh_eeg"
+
