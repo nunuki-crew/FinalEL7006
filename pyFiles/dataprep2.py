9d4e90885e45c4d9dd277f0be150bd0db699d6e8
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-10-27 14:38:58 -0300

    Sorted everything

---
+++ pyFiles/dataprep2.py
@@ -0,0 +1,218 @@
+import os
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+
+class CustomEEGDataset(Dataset):
+    def __init__(self, csv_file , root_dir , transform = None, multi = False, ):
+
+        try:
+            self.loc_df = pd.read_csv(os.path.join(root_dir,csv_file)).drop(labels="Unnamed: 0", axis = 1)
+        except:
+            self.loc_df = pd.read_csv(os.path.join(root_dir,csv_file))
+        # self.loc_df = loc_df
+        self.transform = transform
+        self.root_dir = root_dir
+        self.multi = multi
+    def __len__(self,):
+        return len(self.loc_df)
+        
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        if type(idx) == int:
+            idx = [idx]
+
+        batch = []   
+        for i in idx:
+            eeg_file = os.path.join(self.root_dir,
+                                self.loc_df.iloc[i, 3])
+            # eeg = torch.from_numpy(torch.load(eeg_file)).type(torch.FloatTensor) # [0][0]
+            eeg = torch.load(eeg_file)
+
+            if self.multi:
+                eeg= eeg.unsqueeze(0)
+            batch.append(eeg) 
+        bat = torch.vstack(batch)
+            
+        if self.transform is not None:
+            bat = self.transform(bat)
+
+        return bat
+    
+class DFSpliter():
+    def __init__(self, train_size= 0.8, val_size = 0.2, save = False, seed = 69) -> None:
+        self.train_size = train_size
+        self.val_size = val_size
+        self.save = save
+        self.seed = seed
+
+    def __call__(self, csv_file, root_path):
+        try:
+            loc_df = pd.read_csv(os.path.join(root_path,csv_file)).drop(labels="Unnamed: 0", axis = 1)
+        except:
+            loc_df = pd.read_csv(os.path.join(root_path,csv_file))
+        # loc_df = csv_file
+        patients = loc_df["Patient"].unique()
+        np.random.seed(self.seed)
+        np.random.shuffle(patients)
+        end_idx = round(len(patients)*self.train_size)
+
+        train_patients = patients[:end_idx]
+        val_patients = patients[end_idx:]
+        
+        train_df = pd.DataFrame()
+        for patient in train_patients:
+            train_df = pd.concat([train_df,loc_df[loc_df["Patient"] == patient]])
+
+        val_df = pd.DataFrame()
+        for patient in val_patients:
+            val_df = pd.concat([val_df,loc_df[loc_df["Patient"] == patient]])
+        
+        val_df.reset_index(inplace=True, drop= True)
+        train_df.reset_index(inplace=True, drop=True)
+
+        if self.save:
+            train_df.to_csv("train_feats.csv", encoding= "utf-8", index = False)
+            val_df.to_csv("val_feats.csv", encoding="utf-8", index=False)
+        print("CSVs creados")
+        return train_df,val_df
+    
+def Masking(channel: np.array, window: int= 150):
+    '''
+    Set to zero 
+    Input:  -channel = Numpy array
+            -window = Number of samples to set to zero
+    Output: Numpy array masked
+    '''
+    channel_size = len(channel)
+    first = np.random.randint(0,channel_size- window)
+    masked = channel.copy()
+    masked[first:first+window] = 0
+
+    return masked
+
+def DCVoltage(channel : np.array, max_magnitude: float = 0.5):
+    ''' 
+    Add a DC component between [-max_mangitude, max_magnitude]
+    Input:  -channel = Numpy array
+            -max_magnitude = max value to be added
+    Output: Numpy array 
+    '''
+    dc_comp = (np.random.random(1)*2 - 1)*max_magnitude
+    dispaced_channel = channel + dc_comp
+    return dispaced_channel    
+
+def GaussianNoise(channel: np.array, std: float = 0.2):
+    '''
+    Add Gaussian Noise with zero mean and std deviation
+    Input:  -channel = Numpy array
+            -std = Gaussian std
+    Output: Channel with additive gaussian noise added
+    '''
+    channel_size = len(channel)
+    noise = np.random.normal(loc = 0, scale= std, size= channel_size)
+    noisy_channel = channel + noise
+    return noisy_channel
+
+def Time_Shift(channel: np.array, min_shift: int = 0, max_shift: int = 50 ):
+    ''' 
+    Shifts the channel n samples between min_shift and max_shift using reflection pad
+    Input:  -channel = Numpy array
+            -min_shift = Min number of samples to shift
+            -max_shhift = Max number of samples to shift  
+    Output: Shifted channel
+    '''
+    n_shift = np.random.randint(min_shift,max_shift)
+    channel_size = len(channel)
+    padded_array = np.pad(channel,pad_width= n_shift, mode = "reflect")
+    right_left = np.random.choice((0,2))
+    shifted_array = padded_array[n_shift*right_left:channel_size + n_shift*right_left]
+    return shifted_array
+def Amplitude(channel :np.array, max_amplitude: float = 1.5):
+    '''
+    Modifies the ampliude of the channel values between [1+max_amplitude,1-max_amplitude]
+    Input:  -channel = Numpy array
+            -max_amplitude = Max aplitude to add
+    Output: Boosted channel
+    '''
+    amplitude = 1 + ((np.random.random(1)*2 -1) * max_amplitude)
+    boosted_channel = channel*amplitude
+    return boosted_channel
+
+def Permutation(channel: np.array, win_samples: int = 500):
+    '''
+    Permutates the arrays by secuences of win_samples len
+    Ensure its divisible by the total len of the array or the len of the output secuence will be wrong
+    Input:  -channel = Numpu array
+            -win_samples = Number of samples per secuences (N_sec = len(channel)// win_samples)
+    Output: Permutated secuence
+    '''
+
+    n_seqs = len(channel)// win_samples
+    random_idx = np.random.choice(np.arange(0,n_seqs, 1), n_seqs, replace=False ) 
+    permutated = np.concatenate([channel[win_samples*i: win_samples*(i+1)] for i in random_idx])
+    return permutated
+def Temporal_Invertion(channel: np.array):
+    ''' 
+    Return the array reversed
+    Input:  -channel = Numpy array
+    Output: Reversed array
+    '''
+    reversed = channel[::-1]
+    return reversed
+
+def Negation(channel: np.array):
+    '''
+    Inverts the full array
+    Input: -channel = Numpy array
+    Output: Inverted array
+    '''
+    negated = channel * (-1)
+    return negated
+
+#Augmentation set
+AUGMENTATIONS = [Negation,
+                 Time_Shift,
+                 Amplitude,
+                 DCVoltage,
+                 GaussianNoise,
+                 Temporal_Invertion,
+                 Permutation,
+                 Masking]
+
+class Augmentations(nn.Module):
+    def __init__(self, n_aug, multi = False, augmentations = None) -> None:
+        self.n_aug = n_aug
+        self.multi = multi
+        self.augmentations = augmentations
+
+    def __call__(self,batch):
+        xbar_batch = []
+        xhat_batch = []
+
+        batch = batch.numpy()
+
+        for channel in batch:
+
+            rbar_idxs = np.sort(np.random.choice(np.arange(0,len(self.augmentations),1),size=self.n_aug, replace= False))
+            rhat_idxs = np.sort(np.random.choice(np.arange(0,len(self.augmentations),1),size=self.n_aug, replace= False))
+
+            xbar = channel
+            xhat = channel
+            for i in rbar_idxs:
+                xbar = self.augmentations[i](xbar)
+                # print(self.augmentations[i])
+            for j in rhat_idxs:
+                xhat = self.augmentations[j](xhat)
+                # print(self.augmentations[j])
+            xbar_batch.append(torch.from_numpy(xbar.copy()).type(torch.FloatTensor))
+            xhat_batch.append(torch.from_numpy(xhat.copy()).type(torch.FloatTensor))
+        
+        xbar_batch = torch.vstack(xbar_batch)
+        xhat_batch = torch.vstack(xhat_batch)
+
+        return xbar_batch,xhat_batch
---
+++ pyFiles/training.py
@@ -0,0 +1,224 @@
+"""
+Training functions
+Ammi Beltrán & Fernanda Borja
+"""
+# imports 
+import os
+import pyFiles.dataprep as prep
+import numpy as np
+import torch
+from torch import nn
+from pyFiles.losses import InfoNceLoss
+from pyFiles.losses import BarlowTwins
+
+
+"""
+Pretext
+"""
+def preloss(model, batch, criterion = nn.CrossEntropyLoss(), device = "cuda"):
+    # print(batch.shape)
+    x1, x2 = batch[:, 0:1, :], batch[:, 1:2, :]
+    # print(x1.shape)
+    # x1 = x1.reshape((x1.shape[0],1,-1))#.type(torch.FloatTensor)
+    # x2 = x2.reshape((x2.shape[0],1,-1))#.type(torch.FloatTensor)
+    model = model.to(device)
+    x1, x2 = x1.to(device), x2.to(device)
+    y1, y2 = model(x1), model(x2)
+#     print(y1)
+    #
+    del x1, x2
+    loss = criterion(y1, y2)
+    del y1, y2
+    torch.cuda.empty_cache()
+    return loss
+
+def prevalidate(model, val_loader,criterion):
+    
+    e_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for i, batch in enumerate(val_loader):
+            # aug_batch = augmentation(batch.squeeze())
+            loss= preloss(model, batch, criterion)
+            e_loss +=loss
+            #
+            del loss, batch
+            torch.cuda.empty_cache()
+        e_loss = e_loss/(len(val_loader))
+    return e_loss
+
+def pretrain_epoch(model, train_loader, val_loader, criterion, optimizer):
+    # training mode
+    model.train()
+    vloss = []
+    tloss = []
+    lossSum = 0
+    iters = 0
+    for i, batch in enumerate(train_loader):
+#         print(batch.dtype)
+        # aug_batch = augmentation(batch.squeeze())
+#         print(aug_batch[0].dtype)
+#         print(aug_batch[0][0])
+        loss = preloss(model, batch, criterion)
+        #
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f"Iter: {i + 1}/{len(train_loader)}, Loss:{loss}")
+        lossSum += loss
+        iters += 1
+        #
+        del batch
+        del loss
+        torch.cuda.empty_cache()
+        if ((i+1)%30 == 0):
+            val_loss = prevalidate(model, val_loader, criterion)
+            print(f"Validating, Val loss = {val_loss}")
+            vloss.append(val_loss)
+            tloss.append(lossSum/iters)
+    tloss = lossSum/iters
+    # print(f"Epoch train loss = {trainloss}")
+    return tloss, vloss
+
+def pretext_train(model, epochs, train_loader, val_loader, criterion, optimizer, each = 50, state = None, name = ""):
+    # If trained before
+    if state == None:
+        state = {
+            "epoch" : 0,
+            "loss" : [[], []], # [train, val]
+            "params" : None, 
+            "bestloss" : np.inf
+        }
+    # If previously trained
+    else:
+        state = torch.load(state)
+        model.load_state_dict(state["params"])
+
+    best_loss = state["bestloss"]
+    state_epochs = state["epoch"]
+    for epoch in range(state_epochs, state_epochs + epochs):
+        print(f"Epoch nro {epoch + 1}/{epochs}")
+        # Train
+        train_loss, vloss = pretrain_epoch(model, train_loader, val_loader, criterion, optimizer)
+        # Val
+        val_loss = prevalidate(model, val_loader, criterion)
+        vloss.append(val_loss)
+        # Save if better loss 
+        print(f"Train loss = {train_loss[-1]}, Val loss = {val_loss}")
+
+        if (best_loss>val_loss):
+            best_loss = val_loss
+            print(f"Better params found in epoch = {epoch + 1}, saved params")
+            torch.save(model.state_dict(), f'bestEncoderParams{name}.pt')
+        # Save periodically for each
+        if ((epoch + 1)%each == 0):
+            print(f"Saved epoch multiple of {each}")
+            torch.save(model.state_dict(), f'eachEncoderParams{name}_{epoch + 1}.pt')
+        # Update state
+        state["loss"][0]+=(train_loss)
+        state["loss"][1]+=vloss
+        state["epoch"] = epoch + 1
+        state["params"] = model.state_dict()
+        state["bestloss"] = best_loss
+        # Save last just in case, [includes loss!!!!]
+        torch.save(state, f"Lastencoder{name}_{epoch + 1}.pt")
+    return state["loss"]    
+
+"""
+Downstream
+"""
+
+def downloss(model, batch, criterion = nn.CrossEntropyLoss(), device = "cuda"):
+    x, y = batch
+    model.train()
+    model = model.to(device)
+    x = x.reshape(x.shape[0], 1, -1).type(torch.FloatTensor)
+    x, y = x.to(device), y.to(device)
+    y_pred = model(x)
+    y_pred = (torch.argmax(y_pred, dim = 1)).type(torch.FloatTensor).to(device) # REVISAR FORMATO DE LABEL, POR AHORA ENTREGA 0 o 1, PODRIA SER [0, 1] y [1, 0]
+
+    loss = criterion(y_pred, y)
+    print(loss)
+    return loss, y, y_pred
+
+def downtrain_epoch(model, train_dataset, criterion, optimizer, device = "cuda"):
+    acc = 0
+    e_loss = 0
+    count = 0
+    for i, batch in enumerate(train_dataset):
+        loss, y, y_pred = downloss(model, batch, criterion, device)
+
+        optimizer.zero_grad()
+        loss.requires_grad = True
+        loss.backward()
+        optimizer.step()
+
+        e_loss +=loss
+        acc +=(y == y_pred)
+        count +=1
+        print(f"Iter: {i + 1}/{len(train_dataset)}, Loss:{loss}")
+    acc = acc/count
+    e_loss = e_loss/count
+    # print(f"Epoch train loss = {e_loss}")
+    return e_loss, acc
+
+def downvalidate(model, val_dataset, criterion):
+    acc = 0
+    e_loss = 0
+    count = 0
+    model.eval()
+    with torch.no_grad():
+        for i, batch in enumerate(val_dataset):
+            loss, y, y_pred = downloss(model, batch, criterion)
+            e_loss +=loss
+            acc+=(y==y_pred)
+            count +=1
+        acc = acc/count
+        e_loss = e_loss/count
+    return e_loss, acc
+
+def downtrain(model, epochs, train_dataset, val_dataset, criterion, optimizer, each = 50, state = None, name = ""):
+    # If trained before
+    if state == None:
+        state = {
+            "epoch" : 0,
+            "loss" : [[], []], # [train, val]
+            "acc" : [[], []], # [train, val]
+            "params" : None, 
+            "bestloss" : np.inf
+        }
+    # If previously trained
+    else:
+        state = torch.load(state)
+        model.load_state_dict(state["params"])
+    best_loss = state["bestloss"]
+    for epoch in range(0, epochs):
+        # Train
+        print(f"Epoch nro {epoch +1}/{epochs}")
+        e_loss, e_acc = downtrain_epoch(model, train_dataset, criterion, optimizer)
+        # Validate
+        val_loss, val_acc = downvalidate(model, val_dataset, criterion)
+        #
+        print(f"Epoch {epoch + 1}/{epochs}: Train loss = {e_loss}, Val loss = {val_loss}, Train acc = {e_acc}, Val acc = {val_acc}")
+        # Save if better loss
+        if val_loss < best_loss:
+            best_loss = val_loss
+            print(f"Better params found in epoch = {epoch + 1}, saved params")
+            torch.save(model.state_dict(), f'bestDownParams{name}.pt')
+        # Load best model so far to proceed
+        # model.load_state_dict(torch.load(f'bestDownParams.pt'))
+        # Save periodically for each
+        if ((epoch + 1)%each == 0):
+            print(f"Se ha guardado la época múltiplo de {each}")
+            torch.save(model.state_dict(), f'eachDownParams{name}_{epoch + 1}.pt')
+        # Update state
+        state["loss"][0].append(e_loss)
+        state["loss"][1].append(val_loss)
+        state["acc"][0].append(e_acc)
+        state["acc"][1].append(val_acc)
+        state["epoch"] = epoch + 1
+        state["params"] = model.state_dict()
+        state["bestloss"] = best_loss
+        # Save last just in case, [includes loss!!!!]
+        torch.save(state, f"LastDown{name}_{epoch + 1}.pt")
+        return state["loss"], state["acc"]
